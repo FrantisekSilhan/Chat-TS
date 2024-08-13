17085f0b81b8972e2d1a939e4ee36a2ed6b28a04
MartinRostejnsky <martin.rostejnsky@email.cz>
2024-08-14 00:14:08 +0200

    fix indentation

--- src/routes/api/logout.ts
+++ src/routes/api/logout.ts
@@ -7,21 +7,21 @@ const router = express.Router();
 const path = "/api/logout";
 
 router.post("/", isAuthenticated, async (req, res, next) => {
-    req.session.destroy(err => {
-        if (err) {
-          next(err);
-        } else {
-          res.status(200).json({ message: "Success" });
-        }
-      });
+  req.session.destroy(err => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).json({ message: "Success" });
+    }
+  });
 });
 
 router.post("/all", isAuthenticated, (req, res, next) => {
-    if (deleteUserSession(req.session.userId!)) {
-      res.status(200).json({ message: "Success" });
-    } else {
-      next({ message: "Failed to log out of all devices", status: 500 });
-    }
-  });
+  if (deleteUserSession(req.session.userId!)) {
+    res.status(200).json({ message: "Success" });
+  } else {
+    next({ message: "Failed to log out of all devices", status: 500 });
+  }
+});
 
 export default { path, router };
